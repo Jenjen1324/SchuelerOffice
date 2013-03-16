12bdca397a26a105af63c96087f9a561f446782b
Jenjen1324 <northcodedotno@gmail.com>
2013-03-17 00:21:04 +0100

    Added Load/Save

--- SchülerOffice/Program.cs
+++ SchülerOffice/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SchülerOffice
 {
@@ -14,9 +15,25 @@ namespace SchülerOffice
         [STAThread]
         static void Main(string[] args)
         {
+            if(!Directory.Exists(Data.workingDir))
+            {
+                Directory.CreateDirectory(Data.workingDir);
+            }
+
+            if(!File.Exists(Data.markFile))
+            {
+                FileStream fs = File.Create(Data.markFile);
+                fs.Close();
+                File.WriteAllText(Data.markFile, "<empty></empty>");
+            }
+
+            Data.LoadMarks();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            Data.SaveMarks();
         }
     }
 }
