ac75fb574476d29ff8d05ccf70fd8518b4b6369e
Jenjen1324 <northcodedotno@gmail.com>
2013-03-16 21:48:28 +0100

    Language implementation
    Only testing. Will be implemented in the future

---
+++ SchülerOffice/Language.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchülerOffice
+{
+    class Language
+    {
+        
+
+        public string somestring { get; set; }
+
+        public string classes { get; set; }
+
+        public string marks { get; set; }
+
+        public string mark { get; set; }
+
+        public string markDate { get; set; }
+
+        public string markName { get; set; }
+
+        public string markNote { get; set; }
+
+        public string markPoints { get; set; }
+
+        public static Language LoadFromFile(string file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
