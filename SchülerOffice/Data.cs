46877fdb435bcf8e17e0bf713839894acab7bdcf
Jenjen1324 <northcodedotno@gmail.com>
2013-03-16 21:50:10 +0100

    Data class for storage
    Removed storage stuff from Program.cs

---
+++ SchülerOffice/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchülerOffice
+{
+    public static class Data
+    {
+        internal static List<string> classes = new List<string>();
+
+        internal static List<Mark> marks = new List<Mark>();
+
+        internal static void messageBox(string Title,string Message)
+        {
+            MessageBox.Show(Message, Title);
+        }
+
+        internal static Language lang = new Language();
+    }
+}
