4754198979e13df2e438137e29723f6beab7512c
Jenjen1324 <northcodedotno@gmail.com>
2013-03-28 07:03:38 +0100

    Tried to get a target-mark calculator working
    Is buggy but the princip works

--- SchülerOffice/MarkUtils.Designer.cs
+++ SchülerOffice/MarkUtils.Designer.cs
@@ -28,20 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.comboBox_classes = new System.Windows.Forms.ComboBox();
             this.chart_marks = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox_additionalMark = new System.Windows.Forms.CheckBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton_aM_countsAsMark = new System.Windows.Forms.RadioButton();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton_aM_countsAsPrecentage = new System.Windows.Forms.RadioButton();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox_calculateTargetMark = new System.Windows.Forms.GroupBox();
+            this.button_calcuteTargetMark = new System.Windows.Forms.Button();
+            this.textBox_requiredMark = new System.Windows.Forms.TextBox();
+            this.label_requiredMark = new System.Windows.Forms.Label();
+            this.textBox_targetMark = new System.Windows.Forms.TextBox();
+            this.label_targetMark = new System.Windows.Forms.Label();
+            this.label_averageMark = new System.Windows.Forms.Label();
+            this.textBox_averageMark = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart_marks)).BeginInit();
+            this.groupBox_calculateTargetMark.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBox_classes
@@ -56,30 +65,30 @@
             // 
             // chart_marks
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart_marks.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart_marks.Legends.Add(legend1);
+            chartArea3.Name = "ChartArea1";
+            this.chart_marks.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart_marks.Legends.Add(legend3);
             this.chart_marks.Location = new System.Drawing.Point(271, 12);
             this.chart_marks.Name = "chart_marks";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart_marks.Series.Add(series1);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart_marks.Series.Add(series3);
             this.chart_marks.Size = new System.Drawing.Size(312, 226);
             this.chart_marks.TabIndex = 2;
             this.chart_marks.Text = "chart1";
             // 
-            // checkBox1
+            // checkBox_additionalMark
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 41);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(99, 17);
-            this.checkBox1.TabIndex = 3;
-            this.checkBox1.Text = "Mündliche note";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox_additionalMark.AutoSize = true;
+            this.checkBox_additionalMark.Location = new System.Drawing.Point(12, 71);
+            this.checkBox_additionalMark.Name = "checkBox_additionalMark";
+            this.checkBox_additionalMark.Size = new System.Drawing.Size(99, 17);
+            this.checkBox_additionalMark.TabIndex = 3;
+            this.checkBox_additionalMark.Text = "Mündliche note";
+            this.checkBox_additionalMark.UseVisualStyleBackColor = true;
             // 
             // comboBox2
             // 
@@ -91,26 +100,26 @@
             "2x",
             "3x",
             "4x"});
-            this.comboBox2.Location = new System.Drawing.Point(178, 65);
+            this.comboBox2.Location = new System.Drawing.Point(178, 95);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(57, 21);
             this.comboBox2.TabIndex = 5;
             this.comboBox2.Text = "1x";
             // 
-            // radioButton1
+            // radioButton_aM_countsAsMark
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(19, 66);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(92, 17);
-            this.radioButton1.TabIndex = 6;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Zählt als note:";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton_aM_countsAsMark.AutoSize = true;
+            this.radioButton_aM_countsAsMark.Location = new System.Drawing.Point(19, 96);
+            this.radioButton_aM_countsAsMark.Name = "radioButton_aM_countsAsMark";
+            this.radioButton_aM_countsAsMark.Size = new System.Drawing.Size(92, 17);
+            this.radioButton_aM_countsAsMark.TabIndex = 6;
+            this.radioButton_aM_countsAsMark.TabStop = true;
+            this.radioButton_aM_countsAsMark.Text = "Zählt als note:";
+            this.radioButton_aM_countsAsMark.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(178, 39);
+            this.textBox1.Location = new System.Drawing.Point(178, 69);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(26, 20);
             this.textBox1.TabIndex = 7;
@@ -122,26 +131,26 @@
             "10%",
             "20%",
             "50%"});
-            this.comboBox3.Location = new System.Drawing.Point(178, 92);
+            this.comboBox3.Location = new System.Drawing.Point(178, 122);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(57, 21);
             this.comboBox3.TabIndex = 8;
             this.comboBox3.Text = "10%";
             // 
-            // radioButton2
+            // radioButton_aM_countsAsPrecentage
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(18, 93);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(154, 17);
-            this.radioButton2.TabIndex = 9;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Zählt gegüber zeugnisnote:";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton_aM_countsAsPrecentage.AutoSize = true;
+            this.radioButton_aM_countsAsPrecentage.Location = new System.Drawing.Point(18, 123);
+            this.radioButton_aM_countsAsPrecentage.Name = "radioButton_aM_countsAsPrecentage";
+            this.radioButton_aM_countsAsPrecentage.Size = new System.Drawing.Size(154, 17);
+            this.radioButton_aM_countsAsPrecentage.TabIndex = 9;
+            this.radioButton_aM_countsAsPrecentage.TabStop = true;
+            this.radioButton_aM_countsAsPrecentage.Text = "Zählt gegüber zeugnisnote:";
+            this.radioButton_aM_countsAsPrecentage.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(210, 39);
+            this.textBox2.Location = new System.Drawing.Point(210, 69);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(25, 20);
             this.textBox2.TabIndex = 10;
@@ -149,30 +158,110 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(202, 42);
+            this.label1.Location = new System.Drawing.Point(203, 72);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(10, 13);
             this.label1.TabIndex = 11;
             this.label1.Text = "-";
+            // 
+            // groupBox_calculateTargetMark
+            // 
+            this.groupBox_calculateTargetMark.Controls.Add(this.button_calcuteTargetMark);
+            this.groupBox_calculateTargetMark.Controls.Add(this.textBox_requiredMark);
+            this.groupBox_calculateTargetMark.Controls.Add(this.label_requiredMark);
+            this.groupBox_calculateTargetMark.Controls.Add(this.textBox_targetMark);
+            this.groupBox_calculateTargetMark.Controls.Add(this.label_targetMark);
+            this.groupBox_calculateTargetMark.Location = new System.Drawing.Point(12, 149);
+            this.groupBox_calculateTargetMark.Name = "groupBox_calculateTargetMark";
+            this.groupBox_calculateTargetMark.Size = new System.Drawing.Size(223, 119);
+            this.groupBox_calculateTargetMark.TabIndex = 12;
+            this.groupBox_calculateTargetMark.TabStop = false;
+            this.groupBox_calculateTargetMark.Text = "Zielnote berechnen";
+            // 
+            // button_calcuteTargetMark
+            // 
+            this.button_calcuteTargetMark.Location = new System.Drawing.Point(108, 45);
+            this.button_calcuteTargetMark.Name = "button_calcuteTargetMark";
+            this.button_calcuteTargetMark.Size = new System.Drawing.Size(75, 23);
+            this.button_calcuteTargetMark.TabIndex = 16;
+            this.button_calcuteTargetMark.Text = "Berechnen";
+            this.button_calcuteTargetMark.UseVisualStyleBackColor = true;
+            this.button_calcuteTargetMark.Click += new System.EventHandler(this.button_calcuteTargetMark_Click);
+            // 
+            // textBox_requiredMark
+            // 
+            this.textBox_requiredMark.Location = new System.Drawing.Point(132, 74);
+            this.textBox_requiredMark.Multiline = true;
+            this.textBox_requiredMark.Name = "textBox_requiredMark";
+            this.textBox_requiredMark.ReadOnly = true;
+            this.textBox_requiredMark.Size = new System.Drawing.Size(51, 39);
+            this.textBox_requiredMark.TabIndex = 15;
+            // 
+            // label_requiredMark
+            // 
+            this.label_requiredMark.AutoSize = true;
+            this.label_requiredMark.Location = new System.Drawing.Point(6, 77);
+            this.label_requiredMark.Name = "label_requiredMark";
+            this.label_requiredMark.Size = new System.Drawing.Size(120, 13);
+            this.label_requiredMark.TabIndex = 14;
+            this.label_requiredMark.Text = "Benötigte prüfungsnote:";
+            // 
+            // textBox_targetMark
+            // 
+            this.textBox_targetMark.Location = new System.Drawing.Point(132, 19);
+            this.textBox_targetMark.Name = "textBox_targetMark";
+            this.textBox_targetMark.Size = new System.Drawing.Size(51, 20);
+            this.textBox_targetMark.TabIndex = 13;
+            // 
+            // label_targetMark
+            // 
+            this.label_targetMark.AutoSize = true;
+            this.label_targetMark.Location = new System.Drawing.Point(6, 22);
+            this.label_targetMark.Name = "label_targetMark";
+            this.label_targetMark.Size = new System.Drawing.Size(48, 13);
+            this.label_targetMark.TabIndex = 0;
+            this.label_targetMark.Text = "Zielnote:";
+            // 
+            // label_averageMark
+            // 
+            this.label_averageMark.AutoSize = true;
+            this.label_averageMark.Location = new System.Drawing.Point(9, 46);
+            this.label_averageMark.Name = "label_averageMark";
+            this.label_averageMark.Size = new System.Drawing.Size(70, 13);
+            this.label_averageMark.TabIndex = 13;
+            this.label_averageMark.Text = "Durchschnitt:";
+            // 
+            // textBox_averageMark
+            // 
+            this.textBox_averageMark.Location = new System.Drawing.Point(178, 43);
+            this.textBox_averageMark.Name = "textBox_averageMark";
+            this.textBox_averageMark.ReadOnly = true;
+            this.textBox_averageMark.Size = new System.Drawing.Size(26, 20);
+            this.textBox_averageMark.TabIndex = 14;
             // 
             // MarkUtils
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(595, 381);
+            this.Controls.Add(this.textBox_averageMark);
+            this.Controls.Add(this.label_averageMark);
+            this.Controls.Add(this.groupBox_calculateTargetMark);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton_aM_countsAsPrecentage);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton_aM_countsAsMark);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox_additionalMark);
             this.Controls.Add(this.chart_marks);
             this.Controls.Add(this.comboBox_classes);
             this.Controls.Add(this.label1);
             this.Name = "MarkUtils";
             this.Text = "MarkUtils";
             ((System.ComponentModel.ISupportInitialize)(this.chart_marks)).EndInit();
+            this.groupBox_calculateTargetMark.ResumeLayout(false);
+            this.groupBox_calculateTargetMark.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,13 +271,21 @@
 
         private System.Windows.Forms.ComboBox comboBox_classes;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart_marks;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox_additionalMark;
         private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton_aM_countsAsMark;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton_aM_countsAsPrecentage;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox_calculateTargetMark;
+        private System.Windows.Forms.Button button_calcuteTargetMark;
+        private System.Windows.Forms.TextBox textBox_requiredMark;
+        private System.Windows.Forms.Label label_requiredMark;
+        private System.Windows.Forms.TextBox textBox_targetMark;
+        private System.Windows.Forms.Label label_targetMark;
+        private System.Windows.Forms.Label label_averageMark;
+        private System.Windows.Forms.TextBox textBox_averageMark;
     }
 }
--- SchülerOffice/MarkUtils.cs
+++ SchülerOffice/MarkUtils.cs
@@ -39,6 +39,81 @@ namespace SchülerOffice
                     i++;
                 }
             }
+            textBox_averageMark.Text = Convert.ToString(calculateAverageMark(comboBox_classes.Text));
+        }
+
+        private float calculateAverageMark(string _class)
+        {
+            float averageMark = 0;
+            int i = 0;
+            foreach (Mark m in Data.marks)
+            {
+                if (m._class == _class)
+                {
+                    i++;
+                    averageMark = averageMark + m.mark;
+                }
+            }
+            return averageMark / i;
+        }
+
+        private void button_calcuteTargetMark_Click(object sender, EventArgs e)
+        {
+            //WIP
+            throw new NotImplementedException();
+            if (textBox_targetMark.Text != "")
+            {
+                try
+                {
+                    float target = Convert.ToSingle(textBox_targetMark.Text);
+                    if (target > 6 || target < 1)
+                    {
+                        Data.messageBox("Warnung!", "Ungültige eingabe!");
+                    }
+                    else
+                    {
+                        float total = 0;
+                        int i = 0;
+                        foreach (Mark m in Data.marks)
+                        {
+                            if (m._class == comboBox_classes.Text)
+                            {
+                                i++;
+                                total = total + m.mark;
+                            }
+                        }
+                        if (i != 0)
+                        {
+                            float targetMark = target * i + target - total;
+                            float[] targetMarks = new float[20];
+                            int j = 0;
+                            while (targetMark > 6)
+                            {
+                                total = total + 6;
+                                i++;
+                                targetMark = target * i + target - total;
+                                targetMarks[j] = 6;
+                                if (j == 19)
+                                {
+                                    break;
+                                }
+                                j++;
+                            }
+                            targetMarks[j] = targetMark;
+                            textBox_requiredMark.Text = "";
+                            foreach (float f in targetMarks)
+                            {
+                                
+                                textBox_requiredMark.Text = textBox_requiredMark.Text + Convert.ToString(f) + "\n\r";
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    Data.messageBox("Warnung!", "Ungültige eingabe!");
+                }
+            }
         }
     }
 }
