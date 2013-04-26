dd4558bfa63ef86808106ac779b036d3038f8c7b
Jenjen1324 <northcodedotno@gmail.com>
2013-04-26 13:02:16 +0200

    Revert "Test"
    This reverts commit 0cb4830bdadcd0c22487ecec729125165e1279c3.

---
+++ SchülerOffice/AddClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchülerOffice
+{
+    public partial class AddClass : Form
+    {
+        public string name
+        {
+            get { return textBox1.Text; }
+        }
+
+        public AddClass()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
